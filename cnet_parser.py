573a9dd17ff6b39fa0c8356c4aa8c8973aef4cf3
arielbruner <arielbruner@gmail.com>
2017-08-25 02:41:57 +0300

    Bug fixes in cnet parser, now supports some special characters in vertex names, and a variation of input nodes without a fixed value (which is discarded in both versions)

--- cnet_parser.py
+++ cnet_parser.py
@@ -31,13 +31,13 @@ def parse_cnet(path):
             indices = [int(re.search(r"([0-9]+)[ \t]*=", line).group(1)) for
                        line in section.split("\n")[1:]]
             assert indices == list(range(1, n + 1))
-            names = [re.search(r"=[ \t]*([0-9a-zA-Z_]+)", line).group(1) for
+            names = [re.search(r"=[ \t]*([0-9a-zA-Z_\-\\ ]+)", line).group(1) for
                      line in section.split("\n")[1:]]
             continue
         if ".n" in section:
             # boolean functions
             v_index_str, v_n_args_str, v_args_str = re.search(
-                r"\.n\s+([0-9]+)[ \t]+([0-9]+)((?:[ \t][0-9]+)*)\n", section).groups()
+                r"\.n[ \t]*([0-9]+)[ \t]+([0-9]+)((?:[ \t][0-9]+)*)", section).groups()
             v_index = int(v_index_str) - 1  # cnet indices are 1 based, convert.
             assert v_index == last_v_index + 1
             last_v_index = v_index
